389727993039b6f36e77a4d8eed185363643ac58
Guigp <guilherme.gp85@hotmail.com>
2019-04-17 13:53:35 -0300

    adicionando porta

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("app rodando na porta 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app rodando na porta ${process.env.PORT}`);
 });
